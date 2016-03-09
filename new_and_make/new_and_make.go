a499c59ba9c8d113801bd3cca477311c53811fa9
wusuopubupt <wdxbupt2009@gmail.com>
2016-03-09 02:27:01 -0500

    new file:   new_and_make/new_and_make.go

---
+++ new_and_make/new_and_make.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// new
+	s = new(string)
+	fmt.Println("%s\n", s)
+	fmt.Println(*s)
+
+	// make
+	// 内建函数make(T, args)与new(T)的用途不一样。它只用来创建slice，map和channel，
+	// 并且返回一个初始化的(而不是置零)，类型为T的值（而不是*T）
+	v := make([]int, 100)
+
+}
